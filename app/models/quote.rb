543f78421400741a1a52e88115f313fda8c166a3
Student <example@example.com>
2020-03-05 10:55:15 -0800

    added error handler -broken

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -1,8 +1,2 @@
-class AddQuotesTable < ActiveRecord::Migration[5.0]
-  def change
-    create_table :quotes do |t|
-      t.column :content, :string
-      t.column :author, :string
-    end
-  end
+class Quote < ApplicationRecord
 end
